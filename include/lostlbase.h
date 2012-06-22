5ed2f52c766171ed4a8a5492a0b2c2aa6e9186f4
Jordan Stevens <muthink@gmail.com>
2012-06-22 14:12:52 +0200

    messing around with new buffer link base class.

--- include/lostlbase.h
+++ include/lostlbase.h
@@ -35,7 +35,8 @@ namespace lo
 		ref(const T* obj) : ref_base(obj)	{  }
 		ref(object_base *obj) : ref_base(obj) {}
 		inline ref( ref<T> & external ) : BaseRef(external.ptr) {}
-		inline T* operator->()				{ return (reinterpret_cast<T *>(m_obj)); }
+		inline T *operator->()				{ return ((reinterpret_cast<T *>(m_obj))); }
+		inline const T *operator->() const 	{ return (((const T *)(m_obj))); }
 		inline T *Ptr() const				{ return reinterpret_cast<T*>(m_obj); }
 	private:
 	};
@@ -118,21 +119,26 @@ namespace lo
 		ref<T>	m_pimpl;
 	};
 
-	class _link_buffer : object_base
+	/*!
+	\brief This class contains a pointer and information about the buffer.
+	 */
+	class _link_buffer : public object_base
 	{
 	public:
-	private:
 		char			*m_buffer;
-		size_t			m_sizeOfElement;
-		size_t			m_numberElements;
-		bool			m_isConst;
+		size_t			m_sizeOfElement;	// Byte size of each element.
+		size_t			m_numberElements;	// Number of elements
+		size_t			m_allocatedLength;	// How many bytes are actually allocated
+		bool			m_isConst;			// If true, then this is not deallocated when changed.
 	};
 	/*!
 	\brief This class defines a continuous buffer, which can be either linked
 	or directly allocated.
 	*/
-	class link_buffer
+	class link_buffer : public object_base
 	{
+	protected:
+		ref<_link_buffer>			m_linkBuffer;
 	public:
 		typedef char				value_type;
 		typedef value_type*			pointer;
@@ -145,12 +151,132 @@ namespace lo
 		typedef const_pointer		const_iterator;
 		typedef const_iterator		iterator;
 		typedef const link_buffer&	rcself_t;
-		iterator				begin (void) const;//			{ return m_linkBuffer->m_buffer; }
-		const_pointer		cdata (void) const;//			{ return m_linkBuffer->m_buffer; }
-		bool					empty (void) const;
-		iterator				end (void) const;
-	protected:
-		ref<_link_buffer>			m_linkBuffer;
+		inline iterator				begin (void)			{ return (iterator)(m_linkBuffer->m_buffer); }
+		inline const_pointer		cdata (void) const		{ return (const_pointer)(m_linkBuffer->m_buffer); }
+		bool						empty (void) const;
+		iterator					end (void) const;
+		inline size_type			size (void) const		{ return (size_type)(m_linkBuffer->m_numberElements); }
+	};
+
+#if 0
+	class nstring : public link_buffer
+	{
+	public:
+		typedef char				value_type;
+		typedef value_type*			pointer;
+		typedef const value_type*	const_pointer;
+		typedef wchar_t				wvalue_type;
+		typedef wvalue_type*		wpointer;
+		typedef const wvalue_type*	const_wpointer;
+		typedef pointer				iterator;
+		typedef const_pointer		const_iterator;
+		typedef value_type&			reference;
+		typedef value_type			const_reference;
+		typedef ::std::reverse_iterator<iterator>			reverse_iterator;
+		typedef ::std::reverse_iterator<const_iterator>		const_reverse_iterator;
+		typedef lo::utf8in_iterator<const_iterator>				utf8_iterator;
+		const int npos = -1;
+		nstring& append ( const nstring& str );
+		nstring& append ( const nstring& str, size_t pos, size_t n );
+		nstring& append ( const char* s, size_t n );
+		nstring& append ( const char* s );
+		nstring& append ( size_t n, char c );
+		template <class InputIterator> nstring& append ( InputIterator first, InputIterator last );
+		nstring& assign ( const nstring& str );
+		nstring& assign ( const nstring& str, size_t pos, size_t n );
+		nstring& assign ( const char* s, size_t n );
+		nstring& assign ( const char* s );
+		nstring& assign ( size_t n, char c );
+		template <class InputIterator>
+		nstring& assign ( InputIterator first, InputIterator last );
+		const char& at ( size_t pos ) const;
+		char& at ( size_t pos );
+		iterator begin();
+		const_iterator begin() const;
+		size_t capacity ( ) const;
+		void clear();
+		int compare ( const nstring& str ) const;
+		int compare ( const char* s ) const;
+		int compare ( size_t pos1, size_t n1, const nstring& str ) const;
+		int compare ( size_t pos1, size_t n1, const char* s) const;
+		int compare ( size_t pos1, size_t n1, const nstring& str, size_t pos2, size_t n2 ) const;
+		int compare ( size_t pos1, size_t n1, const char* s, size_t n2) const;
+		size_t copy ( char* s, size_t n, size_t pos = 0) const;
+		const char* c_str ( ) const;
+		const char* data() const;
+		bool empty ( ) const;
+		iterator end();
+		const_iterator end() const;
+		nstring& erase ( size_t pos = 0, size_t n = npos );
+		iterator erase ( iterator position );
+		iterator erase ( iterator first, iterator last );
+		size_t find ( const nstring& str, size_t pos = 0 ) const;
+		size_t find ( const char* s, size_t pos, size_t n ) const;
+		size_t find ( const char* s, size_t pos = 0 ) const;
+		size_t find ( char c, size_t pos = 0 ) const;
+		size_t find_first_not_of ( const nstring& str, size_t pos = 0 ) const;
+		size_t find_first_not_of ( const char* s, size_t pos, size_t n ) const;
+		size_t find_first_not_of ( const char* s, size_t pos = 0 ) const;
+		size_t find_first_not_of ( char c, size_t pos = 0 ) const;
+		size_t find_first_of ( const nstring& str, size_t pos = 0 ) const;
+		size_t find_first_of ( const char* s, size_t pos, size_t n ) const;
+		size_t find_first_of ( const char* s, size_t pos = 0 ) const;
+		size_t find_first_of ( char c, size_t pos = 0 ) const;
+		size_t find_last_of ( const nstring& str, size_t pos = npos ) const;
+		size_t find_last_of ( const char* s, size_t pos, size_t n ) const;
+		size_t find_last_of ( const char* s, size_t pos = npos ) const;
+		size_t find_last_of ( char c, size_t pos = npos ) const;
+		nstring& insert ( size_t pos1, const nstring& str );
+		nstring& insert ( size_t pos1, const nstring& str, size_t pos2, size_t n );
+		nstring& insert ( size_t pos1, const char* s, size_t n);
+		nstring& insert ( size_t pos1, const char* s );
+		nstring& insert ( size_t pos1, size_t n, char c );
+		iterator insert ( iterator p, char c );
+		void insert ( iterator p, size_t n, char c );
+		template<class InputIterator>
+		void insert ( iterator p, InputIterator first, InputIterator last );
+		size_t length() const;
+		size_t max_size ( ) const;
+		nstring& operator+= ( const nstring& str );
+		nstring& operator+= ( const char* s );
+		nstring& operator+= ( char c );
+		nstring& operator= ( const nstring& str );
+		nstring& operator= ( const char* s );
+		nstring& operator= ( char c );
+		const char& operator[] ( size_t pos ) const;
+		char& operator[] ( size_t pos );
+		void push_back ( char c );
+		reverse_iterator rbegin();
+		const_reverse_iterator rbegin() const;
+		reverse_iterator rend();
+		const_reverse_iterator rend() const;
+		nstring& replace ( size_t pos1, size_t n1,   const nstring& str );
+		nstring& replace ( iterator i1, iterator i2, const nstring& str );
+		nstring& replace ( size_t pos1, size_t n1, const nstring& str, size_t pos2, size_t n2 );
+		nstring& replace ( size_t pos1, size_t n1,   const char* s, size_t n2 );
+		nstring& replace ( iterator i1, iterator i2, const char* s, size_t n2 );
+		nstring& replace ( size_t pos1, size_t n1,   const char* s );
+		nstring& replace ( iterator i1, iterator i2, const char* s );
+		nstring& replace ( size_t pos1, size_t n1,   size_t n2, char c );
+		nstring& replace ( iterator i1, iterator i2, size_t n2, char c );
+		template<class InputIterator>
+		nstring& replace ( iterator i1, iterator i2, InputIterator j1, InputIterator j2 );
+		void resize ( size_t n, char c );
+		void resize ( size_t n );
+		size_t rfind ( const nstring& str, size_t pos = npos ) const;
+		size_t rfind ( const char* s, size_t pos, size_t n ) const;
+		size_t rfind ( const char* s, size_t pos = npos ) const;
+		size_t rfind ( char c, size_t pos = npos ) const;
+		size_t size() const;
+		nstring substr ( size_t pos = 0, size_t n = npos ) const;
+		void swap ( nstring& str );
+	};
+#endif
+	template<typename T>
+	class nvector : public link_buffer
+	{
+	public:
+
 	};
 
 	/// The alignment performed by default.
@@ -196,32 +322,32 @@ namespace lo
 		typedef const_iterator		iterator;
 		typedef const cblocklink&		rcself_t;
 	public:
-		inline				cblocklink (void)				: m_Data (NULL), m_Size (0) { }
-							cblocklink (const void* p, size_type n);
-		inline				cblocklink (const cblocklink& l)		: m_Data (l.m_Data), m_Size (l.m_Size) {}
-		inline virtual     ~cblocklink (void) throw()			{}
-		inline iterator		begin (void) const			{ return (iterator (cdata())); }
+		inline					cblocklink (void)				: m_Data (NULL), m_Size (0) { }
+		cblocklink (const void* p, size_type n);
+		inline					cblocklink (const cblocklink& l) : m_Data (l.m_Data), m_Size (l.m_Size) {}
+		inline virtual			~cblocklink (void) throw()	{}
+		inline iterator			begin (void) const			{ return (iterator (cdata())); }
 		inline const_pointer	cdata (void) const			{ return (m_Data); }
-		inline bool			empty (void) const			{ return (!size()); }
-		inline iterator		end (void) const			{ return (iat (size())); }
-		iterator			iat (size_type i) const;
-		void				link (const void* p, size_type n);
-		inline void			link (const cblocklink& l)	{ link (l.begin(), l.size()); }
-		void				link (const void* first, const void* last);
-		inline size_type	max_size (void) const		{ return (size()); }
-		inline rcself_t		operator= (const cblocklink& l)	{ link (l); return (*this); }
-		bool				operator== (const cblocklink& l) const;
-		void				read (std::istream&);
-		inline void			relink (const void* p, size_type n);
-		inline size_type	readable_size (void) const	{ return (size()); }
-		inline void			resize (size_type n)		{ m_Size = n; }
-		inline size_type	size (void) const			{ return (m_Size); }
-		size_type			stream_size (void) const;
-		void				swap (cblocklink& l);
-		void				text_write (std::ostringstream& os) const;
-		virtual void		unlink (void) throw();
-		void				write (std::ostream& os) const;
-		void				write_file (const char* filename, int mode = 0644) const;
+		inline bool				empty (void) const			{ return (!size()); }
+		inline iterator			end (void) const			{ return (iat (size())); }
+		iterator				iat (size_type i) const;
+		void					link (const void* p, size_type n);
+		inline void				link (const cblocklink& l)	{ link (l.begin(), l.size()); }
+		void					link (const void* first, const void* last);
+		inline size_type		max_size (void) const		{ return (size()); }
+		inline rcself_t			operator= (const cblocklink& l)	{ link (l); return (*this); }
+		bool					operator== (const cblocklink& l) const;
+		void					read (std::istream&);
+		inline void				relink (const void* p, size_type n);
+		inline size_type		readable_size (void) const	{ return (size()); }
+		inline void				resize (size_type n)		{ m_Size = n; }
+		inline size_type		size (void) const			{ return (m_Size); }
+		size_type				stream_size (void) const;
+		void					swap (cblocklink& l);
+		void					text_write (std::ostringstream& os) const;
+		virtual void			unlink (void) throw();
+		void					write (std::ostream& os) const;
+		void					write_file (const char* filename, int mode = 0644) const;
 	private:
 		const_pointer	m_Data;		///< Pointer to the data block (const)
 		size_type		m_Size;		///< size of the data block
