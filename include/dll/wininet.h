1e7b77ff66727ecf622769b42b5ee905a8bd3ac4
Jordan Stevens <muthink@gmail.com>
2012-07-03 16:44:52 +0200

    wininet.h

---
+++ include/dll/wininet.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "lolibbase.h"
+#include <dll.h>
+
+#ifdef __cplusplus
+namespace lo { namespace wininet {
+#endif
+
+
+#ifdef __cplusplus
+} } // end namespace lo::psapidll
+
+using namespace lo::wininet;
+
+extern "C" {
+#endif // __cplusplus
+
+
+
+#ifdef __cplusplus
+};
+#endif / __cplusplus
